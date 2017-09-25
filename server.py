a1469204f85fe94dcd9f9561f3d794f8f68f5d52
Amanda <amreinhard@icloud.com>
2017-09-25 21:05:34 +0000

    Possible working version of FB Graph event API

--- server.py
+++ server.py
@@ -1,22 +1,23 @@
-import requests, json
+import requests
+import urllib3
+import facebook
+import os
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
-# GET /oauth/access_token
-#     ?client_id=os.environ['FACEBOOK_APP_ID']
-#     &client_secret=os.environ['FACEBOOK_APP_SECRET']
-#     &grant_type=client_credentials
-#how do I use this?
-
 #https://developers.facebook.com/docs/graph-api/reference/event/
+#https://medium.com/towards-data-science/how-to-use-facebook-graph-api-and-extract-data-using-python-1839e19d6999
 
 app = Flask(__name__)
-app.secret_key = os.environ['FACEBOOK_APP_SECRET']
 
-event_info = graph.get_object(id='event_id',
-                              fields='id, start_time, is_canceled, owner, parent_group, attending_count, category, guest_list_enabled, description')
+api = facebook.Api(
+    app_id=os.environ['FACEBOOK_APP_ID'],
+    secret_key=os.environ['FACEBOOK_APP_SECRET'],
+    user_token=os.environ['FACEBOOK_USER_TOKEN']
+)
 
-#GET graph.facebook.com/search?q=user_query&type=event
+graph = facebook.GraphAPI(access_token=user_token, version=2.7)
+
 
 @app.route("/")
 def homepage():
@@ -24,16 +25,23 @@ def homepage():
 
     return render_template("homepage.html")
 
+
 @app.route("/event-search")
 def render_event_form():
     """Renders event finder form."""
 
     return render_temploate("event-search.html")
 
+
 @app.route("/upcoming-events")
 def plot_events():
     """Finds and returns upcoming events from Facebook."""
 
-    event = request.args.get("/graph.facebook.com/search?q=party&type=event")
+    events = graph.request("/search?q=party&type=event&limit=100")
+    eventList = events['data']
+    eventid = eventList[1]['id']
 
-    return event
+    event1 = graph.get_object(id=eventid, fields='fields=’attending_count,can_guests_invite,category,cover,declined_count,description,end_time,guest_list_enabled,interested_count,is_canceled,is_page_owned,is_viewer_admin,maybe_count,noreply_count,owner,parent_group,place,ticket_uri,timezone,type,updated_time')
+    attenderscount = event1['attending_count']
+
+    return event1, attenderscount
