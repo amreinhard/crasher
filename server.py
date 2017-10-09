2ca8d590eb607597ab5f84e96ac907001100876a
Amanda <amreinhard@icloud.com>
2017-10-09 23:57:38 +0000

    Refactoring cleanup pre-review.

--- server.py
+++ server.py
@@ -25,57 +25,80 @@ def render_event_form():
 
 @app.route("/upcoming-events", methods=["GET"])
 def grab_events():
-    """Gets from HTML, grabs events from Graph API."""
+    """Gets from HTML, grabs and returns events from Graph API."""
+    #fixed
 
     event_keyword = request.args.get('event-keyword').strip().replace(" ", "%20")
     search_location = request.args.get('city').strip().lower()
-    #use sets
-    events = graph.request("/search?q=" + event_keyword + "&type=event&limit=10000&since=" + str(current_time), post_args={'method': 'get'}) 
-    #rewrite to offset, store in sets, assume duplication
-    return events
+
+    events = graph.request("/search?q=" + event_keyword + "&type=event&limit=10000", post_args={'method': 'get'}) 
+    return events, search_location
+
+def paginate():
+    """Allows next page of results from Graph."""
+        #how do I tie pagination in with grab_events?
+
+    for paging in check_pagination:
+       # # check_cursors = check_pagination.get('cursors', {})
+       #  check_after = check_cursors.get('after', '')
+       # # check_before = check_cursors.get('before', '')
+       #  check_next = check_pagination.get('next', '')
+        if check_after != '':
+            next_page = graph.request("/search?access_token=" + user_token + "&since=" + current_time + "&q=" + event_keyword + "&type=event&limit=1000&after=" + check_after)
+            #probably need to rewrite this at some point^
+    return render_template("/search-results.html", )
 
 
-def input_checks():
+def input_checks(event_keyword, search_location, events):
     """Performs checks on nested dicts, var existence/match checks."""
 
-    event_dict = {}
+    event_by_city = set()
 
     if event_keyword and search_location:
         for individual_events in events['data']:
+            event_id = individual_events['id']
             check_place = individual_events.get('place', {})
             check_location = check_place.get('location', {})
             check_city = check_location.get('city', '')
             if check_city.lower() == search_location:
-                #put IDs of events into event_dict
+                event_by_city.append(event_id)
     else:
         flash("You have to fill in both fields!")
         return redirect("/event-search")
 
-    return event_dict
+    return event_by_city
 
-def event_details():
+
+def event_details(event_by_city):
     """Makes batch request to Graph for event details."""
 
-    event_data = graph.get_object(id=event_dict,
+    event_data = graph.get_object(id=event_by_city,
                                       fields='attending_count, \
         category,start_time,end_time,interested_count,is_canceled, \
-        is_page_owned,maybe_count,ticket_uri,timezone,type')
+        is_page_owned,ticket_uri,timezone,type')
     return event_data
 
-def detail_checks():
+
+def detail_checks(event_data):
     """Checks to make sure attending_count, etc fit parameters."""
-        results_dict = {}
+
+    results_dict = {}
         #do I need to move/declare this individual_events-relevant stuff in 
         #input_checks()? or is there another way?
-        if event_data['attending_count'] <= 75 and \
-            event_data['is_canceled'] is False and \
+    if event_data['attending_count'] <= 75 and \
+        event_data['is_canceled'] is False and \
             event_data['is_page_owned'] is False:
-            results_dict['url'] = "https://www.facebook.com/events/" + str(event_id) + "/"
-            results_dict['event_name'] = individual_events['name']
-            results_dict['event_description'] = individual_events['description']
+        results_dict['url'] = "https://www.facebook.com/events/" + str(#ID OF EVENT) + "/"
+        #based on how I've rewritten input_checks(), how do I get event_id?
+        results_dict['event_name'] = individual_events['name']
+        results_dict['event_description'] = individual_events['description'] #syntax error on this line? why?
+        event_set = set()
+        event_set.add(events)
+
+    return event_set
 
 
-
+# need to find places for all this:
     #                 for pkeys, pvals in individual_events['place'].iteritems():
     #                     if str(pkeys) == 'location':
     #                         for lkeys, lvals in pvals.iteritems():
@@ -86,30 +109,18 @@ def detail_checks():
     #                 for keys, values in event_data.iteritems():
     #                     results_dict[keys] = values
 
-
-    # if results_dict == {}:
-    #     flash("No results found.")
-    #     return redirect('/event-search')
-
-    # json.dump(results_dict, open('event-results.json', 'w'))
     # json.dump(check_pagination, open('pagination.json', 'w'))
     # pprint(results_dict)
 
-    #https://en.wikipedia.org/wiki/ISO_8601 - FB returns time like the example
+def result_return():
+    """Handles edge cases, returns results."""
 
-def paginate():
-    """Allows next page of results from Graph."""
-        #how do I utilize pagination? 
+    if results_dict == {}:
+        flash("No results found.")
+        return redirect('/event-search')
 
-    for paging in check_pagination:
-       # check_cursors = check_pagination.get('cursors', {})
-        check_after = check_cursors.get('after', '')
-       # check_before = check_cursors.get('before', '')
-        check_next = check_pagination.get('next', '')
-        if check_after != '':
-            next_page = graph.request("/search?access_token=" + user_token + "&since=" + current_time + "&q=" + event_keyword + "&type=event&limit=1000&after=" + check_after)
+    return render_template("/search-results")
 
-    return render_template("/search-results.html", )
 
 @app.route("/process-json")
 def jconvert():
