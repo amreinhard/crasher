f1df71823b6cb4dbd0a99b14a6b2fb7c6adb1c03
Amanda <amreinhard@icloud.com>
2017-10-09 20:04:32 +0000

    Trying to refactor.

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 app.secret_key = "ultra secret ssssh"
 user_token = os.environ['FACEBOOK_USER_TOKEN']
+gmaps_key = os.environ['GOOGLE_MAPS_TOKEN']
 graph = facebook.GraphAPI(access_token=user_token, version=2.10)
-
 
 @app.route("/")
 def homepage():
@@ -24,31 +24,49 @@ def render_event_form():
 
 
 @app.route("/upcoming-events", methods=["GET"])
-def plot_events():
-    """Finds and returns upcoming events from Facebook."""
+def grab_events():
+    #use sets
+    events = graph.request("/search?q=" + event_keyword + "&type=event&limit=1000&since=" + str(current_time), post_args={'method': 'get'}) 
+        #rewrite to offset, store in sets, assume duplication
+        #write a loop that handles the sorting here
+    return events
 
-    current_time = int(time.time())
+
+def plot_events():
+    """Handles HTML inputs, cleanup and other random variables."""
+
+    current_time = int(time.time()) #do I even need this with offset?
     event_keyword = request.args.get('event-keyword').strip().replace(" ", "%20")
     search_location = request.args.get('city').strip().lower()
     results_dict = {}
-    counter = 0
-    events = graph.request("/search?q=" + event_keyword + "&type=event&limit=1000&since=" + str(current_time), post_args={'method': 'get'})
-    check_pagination = events.get('paging', {})
-    #how do I utilize pagination? 
+
+
+def input_checks():
+    """Performs checks on nested dicts, var existence/match checks."""
+
+    event_dict = {}
 
     if event_keyword and search_location:
         for individual_events in events['data']:
-            counter += 1
-            event_id = individual_events['id']  # ID NEEDED TO ACCESS EVENTS
             check_place = individual_events.get('place', {})
             check_location = check_place.get('location', {})
             check_city = check_location.get('city', '')
-
             if check_city.lower() == search_location:
-                event_data = graph.get_object(id=event_id,
-                                              fields='attending_count, \
-                category,start_time,end_time,interested_count,is_canceled, \
-                is_page_owned,maybe_count,ticket_uri,timezone,type')
+                #put IDs of events into event_dict
+    else:
+        flash("You have to fill in both fields!")
+        return redirect("/event-search")
+
+    return check_city
+
+def 
+
+    
+        event_id = individual_events['id']  # id used for composing events URL
+        event_data = graph.get_object(id=event_id,
+                                      fields='attending_count, \
+        category,start_time,end_time,interested_count,is_canceled, \
+        is_page_owned,maybe_count,ticket_uri,timezone,type')
                 if event_data['attending_count'] <= 75 and \
                     event_data['is_canceled'] is False and \
                         event_data['is_page_owned'] is False:
@@ -68,9 +86,6 @@ def plot_events():
                     continue
             else:
                 continue
-    else:
-        flash("You have to fill in both fields!")
-        return redirect("/event-search")
 
     print counter
 
@@ -79,25 +94,14 @@ def plot_events():
         return redirect('/event-search')
 
     json.dump(results_dict, open('event-results.json', 'w'))
+    json.dump(check_pagination, open('pagination.json', 'w'))
     pprint(results_dict)
 
-    return check_pagination, redirect("/pagination", check_pagination=check_pagination)
+    #https://en.wikipedia.org/wiki/ISO_8601 - FB returns time like the example
 
-
-@app.route("/process-json")
-def jconvert():
-    """Turns JSON to JS obj."""
-
-    processor = open('event-results.json', 'r')
-    json_string = processor.read()
-    processor.close()
-    data = json.loads(json_string)
-    return jsonify(data)
-
-
-@app.route('/pagination')
 def paginate():
     """Allows next page of results from Graph."""
+        #how do I utilize pagination? 
 
     for paging in check_pagination:
        # check_cursors = check_pagination.get('cursors', {})
@@ -108,6 +112,16 @@ def paginate():
             next_page = graph.request("/search?access_token=" + user_token + "&since=" + current_time + "&q=" + event_keyword + "&type=event&limit=1000&after=" + check_after)
 
     return render_template("/search-results.html", )
+
+@app.route("/process-json")
+def jconvert():
+    """Turns JSON to JS obj."""
+
+    processor = open('event-results.json', 'r')
+    json_string = processor.read()
+    processor.close()
+    data = json.loads(json_string)
+    return jsonify(data)
 
 
 ### Helper Functions ####
