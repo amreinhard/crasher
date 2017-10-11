4a78de73e80418a8c409f5d8f730bdfb9ae15898
Amanda <amreinhard@icloud.com>
2017-10-11 18:29:02 +0000

    Server file fixed.

--- server.py
+++ server.py
@@ -30,18 +30,31 @@ def grab_events():
 
     event_keyword = request.args.get('event-keyword').strip().replace(" ", "%20")
     search_location = request.args.get('city').strip().lower()
+    events = graph.request("/search?q=" + event_keyword + "&type=event&limit=1000", post_args={'method': 'get'})
+    returned_events = []
 
-    events = graph.request("/search?q=" + event_keyword + "&type=event&limit=10", post_args={'method': 'get'})
-    return jsonify(events), search_location, event_keyword
+    while events:
+        event_list = input_checks(event_keyword, search_location, events)
+        event_list = event_details(event_list)
+        checker = detail_checks(event_list, events)
+        returned_events.extend(checker)
+        events = paginate(events, event_keyword)
+        if not events['data']:
+            #check for paging token
+            flash("No more results.")
+            break
+
+    pprint(returned_events)
+    return render_template("/search-results.html")
 
 
 def paginate(events, event_keyword):
     """Allows next page of results from Graph."""
-        #how do I tie pagination in with grab_events?
-    check_pagination = events.get('paging', {})
-    print check_pagination
 
-    if check_pagination != {}:
+    check_pagination = events.get('paging', {})
+    next_page = None
+
+    if check_pagination:
         for paging in check_pagination:
             check_cursors = check_pagination.get('cursors', {})
             check_after = check_cursors.get('after', '')
@@ -49,14 +62,13 @@ def paginate(events, event_keyword):
             check_next = check_pagination.get('next', '')
             if check_after != '':
                 next_page = graph.request("/search?access_token=" + user_token + "&q=" + event_keyword + "&type=event&limit=1000&after=" + check_after)
-            else:
-                flash("End of results.")
 
-    return render_template("/search-results.html", next_page=next_page)
+    return next_page
 
 
 def input_checks(event_keyword, search_location, events):
-    """Performs checks on nested dicts, var existence/match checks."""
+    """Performs checks on nested dicts, var existence/match checks. \
+    Checks city, if city matches, appends id to list."""
 
     event_by_city = []
 
@@ -78,10 +90,10 @@ def input_checks(event_keyword, search_location, events):
 def event_details(event_by_city):
     """Makes batch request to Graph for event details."""
 
-    event_data = graph.get_object(id=event_by_city,
-                                      fields='attending_count, \
-        category,start_time,end_time,interested_count,is_canceled, \
-        is_page_owned,ticket_uri,timezone,type')
+    event_data = graph.get_objects(ids=event_by_city,
+                                   fields='attending_count, \
+        category,description,start_time,end_time,interested_count,is_canceled, \
+        is_page_owned,name,place,ticket_uri,timezone,type')
     return event_data
 
 
@@ -90,40 +102,36 @@ def detail_checks(event_data, events):
     set to be returned to user."""
 
     results_dictionary = {}
-        #is this impractical? do I only need to run through events['data'] in input_checks()?
-    for individual_events in events['data']:
-        if event_data['attending_count'] <= 75 and \
-            event_data['is_canceled'] is False and \
-                event_data['is_page_owned'] is False:
-            results_dictionary['url'] = "https://www.facebook.com/events/"# + str(ID OF EVENT) + "/"
+    event_list = []
+
+    for individual_event in event_data.values():
+        if individual_event['attending_count'] <= 75 and \
+            individual_event['is_canceled'] is False and \
+                individual_event['is_page_owned'] is False:
+            results_dictionary['url'] = "https://www.facebook.com/events/"  # + str(ID OF EVENT) + "/"
             #based on how I've rewritten input_checks(), how do I get individual event ids?
-            results_dictionary['event_name'] = individual_events['name']
-            results_dictionary['event_description'] = individual_events['description']
-            event_set = set()
-            event_set.add(individual_events)
+            for pkeys, pvals in individual_event['place'].iteritems():
+                if str(pkeys) == 'location':
+                    for lkeys, lvals in pvals.iteritems():
+                        results_dictionary[lkeys] = lvals
+                else:
+                    results_dictionary[pkeys] = pvals
+                #else bit prints event owner data, if unpacks location
+            for keys, values in individual_event.iteritems():
+                results_dictionary[keys] = values
+            event_list.append(results_dictionary)
 
-    return event_set, results_dictionary
+    return event_list
 
 
-# need to find places for all this:
-    #                 for pkeys, pvals in individual_events['place'].iteritems():
-    #                     if str(pkeys) == 'location':
-    #                         for lkeys, lvals in pvals.iteritems():
-    #                             results_dict[lkeys] = lvals
-    #                     else:
-    #                         results_dict[pkeys] = pvals
-    #                     #else bit prints event owner data, if unpacks location
-    #                 for keys, values in event_data.iteritems():
-    #                     results_dict[keys] = values
-
-def result_return(results_dictionary):
+def result_return(event_set):
     """Handles edge cases, returns results."""
 
-    if results_dictionary == {}:
+    if event_set == set():
         flash("No results found.")
         return redirect('/event-search')
 
-    return render_template("/search-results")
+    return render_template("/search-results", event_set=event_set)
 
 
 @app.route("/process-json")
